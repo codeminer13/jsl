cef8c3989f7a2da3aae0f09812b4627645997cbe
Navjot Singh <navjotsingh84@gmail.com>
2022-12-03 01:31:50 +0530

    Update build

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,3 +1,5 @@
 @Library('pipleline-library-demo')_
 
-buildJavascriptApp deploy: true
+buildJavascriptApp deploy: false, {
+        notify type: "slack", message: "Build succeeded"
+    }
--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -1,19 +1,17 @@
-def call() {
+def call(Map config=[:], Closure body) {
     node {
-        checkout scm
-
+        git url: "https://github.com/werne2j/sample-nodejs"
         stage("Install") {
             sh "npm install"
         }
-
         stage("Test") {
             sh "npm test"
         }
-
         stage("Deploy") {
             if (config.deploy) {
                 sh "npm publish"
             }
         }
+        body()
     }
 }
